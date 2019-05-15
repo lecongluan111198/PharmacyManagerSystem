e9b165c73f7d29ab9bdc8c54562a6680327a02ca
LuanLee <1612360@student.hcmus.edu.vn>
2019-05-15 23:33:44 +0700

    test

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,15 @@ module.exports = {
         '@vue/app'
     ]
 }
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAPagG5GsYkHomhq6yY9cgUhcf2p-t5SgA",
+    authDomain: "pharmacy-1bb85.firebaseapp.com",
+    databaseURL: "https://pharmacy-1bb85.firebaseio.com",
+    projectId: "pharmacy-1bb85",
+    storageBucket: "pharmacy-1bb85.appspot.com",
+    messagingSenderId: "655297523570",
+    appId: "1:655297523570:web:03e3c7eae59fc2d9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
