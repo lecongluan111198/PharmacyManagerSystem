aa363c7fe5ed713bbd247b320eca6f0e2d8bd7fa
LuanLee <1612360@student.hcmus.edu.vn>
2019-05-27 21:40:23 +0700

    update receipt controller

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Prescription;
-use App\Medicine;
+use App\Receipt;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class PrescriptionController extends Controller
+class ReceiptController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,17 +14,11 @@ class PrescriptionController extends Controller
      */
     public function index(Request $request)
     {
-        $sort_direction = $request->get("direction", "asc");    
-        $sort_key = $request->get("sort", "id");
-        $search = $request->get("q", "");
-        $start = $request->get("start", $date = date('m/d/Y',strtodate('01/01/2019')));
+        $start = $request->get("start", $date = date('m/d/Y', strtodate('01/01/2019')));
         $end = $request->get("end", $date = date('m/d/Y', time()));
-        $items = Medicine::query()
-            ->where("id", "LIKE", "%$search%")
-            ->orWhere("name", "LIKE", "%$search%")
-            ->where("invoiceDate", ">=", $start)
-            ->where("invoiceDate", "<=", $end)
-            ->orderBy($sort_key, $sort_direction)
+        $items = Receipt::query()
+            ->where("receiptDate", ">=", $start)
+            ->where("receiptDate", "<=", $end)
             ->paginate(15);
         return response()->json($items);
     }
@@ -34,11 +26,11 @@ class PrescriptionController extends Controller
     public function getId($id)
     {
         try {
-            $prescription = Prescription::findOrFaile($id);
-            $medicines = $prescription->medicines()->get();
+            $receipt = Receipt::findOrFaile($id);
+            $medicines = $receipt->medicines()->get();
             $ret = [
                 'success' => true,
-                'prescription' => $prescription,
+                'prescription' => $receipt,
                 'medicines' => $medicines
             ];
         } catch (ModelNotFoundException $ex) {
@@ -68,20 +60,21 @@ class PrescriptionController extends Controller
      */
     public function store(Request $request)
     {
-        $prescription = new Prescription();
+        $receipt = new Receipt();
         //tao id
-        $prescription->name = $request->name;
-        $prescription->invoiceDate = $request->invoiceDate;
-        $prescription->cost = $request->cost;
-        $prescription->idPharma = $request->idPharma;
+        $receipt->name = $request->name;
+        $receipt->receiptDate = $request->receiptDate;
+        $receipt->cost = $request->cost;
+        $receipt->note = $request->note;
+        $receipt->type = $request->type;
 
         $medicines = Medicine::findMany($request->listIds);
-        $prescription->medicines()->attach($medicines);
-        if ($prescription->save()) {
+        $receipt->medicines()->attach($medicines);
+        if ($receipt->save()) {
             $ret = [
                 'success' => true,
                 'message' => 200,
-                'prescription' => $prescription
+                'prescription' => $receipt
             ];
         } else {
             $ret = [
@@ -93,42 +86,13 @@ class PrescriptionController extends Controller
         return response()->json($ret);
     }
 
-    public function addMedicine($id, $medi_id)
-    {
-        // try {
-        //     $prescription = Prescription::findOrFail($id);
-        //     $medicines = $prescription->medicines()->attach($medi_id);
-
-        //     if ($prescription->save()) {
-        //         $ret = [
-        //             'success' => true,
-        //             'message' => 200,
-        //             'prescription' => $prescription
-        //         ];
-        //     } else {
-        //         $ret = [
-        //             'success' => false,
-        //             'message' => 404,
-        //             'prescription' => null
-        //         ];
-        //     }
-        // } catch (ModelNotFoundException $ex) {
-        //     $ret = [
-        //         'success' => false,
-        //         'message' => $ex->getMessage(),
-        //         'medicine' => null
-        //     ];
-        // }
-        // return response()->json($ret);
-    }
-
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Receipt  $receipt
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Receipt $receipt)
     {
         //
     }
@@ -136,15 +100,15 @@ class PrescriptionController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Receipt  $receipt
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        $prescription =  Prescription::findOrFail($id);
-        $medicines = $prescription->medicines()->get();
+        $receipt =  Receipt::findOrFail($id);
+        $medicines = $receipt->medicines()->get();
         return view('update', [
-            'prescription' => $prescription,
+            'prescription' => $receipt,
             'medicines' => $medicines
         ]);
     }
@@ -153,28 +117,29 @@ class PrescriptionController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Receipt  $receipt
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
     {
         try {
-            $prescription = Prescription::findOrFail($request->id);
-            $prescription->name = $request->name;
-            $prescription->cost = $request->cost;
-            $prescription->idCate = $request->idCate;
-            $prescription->idProvider = $request->idProvider;
-            if ($prescription->save()) {
+            $receipt = Receipt::findOrFail($request->id);
+            $receipt->name = $request->name;
+            $receipt->receiptDate = $request->receiptDate;
+            $receipt->cost = $request->cost;
+            $receipt->note = $request->note;
+            $receipt->type = $request->type;
+            if ($receipt->save()) {
                 $ret = [
                     'success' => true,
                     'message' => 200,
-                    'medicine' => $prescription,
+                    'medicine' => $receipt,
                 ];
             } else {
                 $ret = [
                     'success' => true,
                     'message' => 404,
-                    'medicine' => $prescription,
+                    'medicine' => $receipt,
                 ];
             }
         } catch (ModelNotFoundException $ex) {
@@ -190,13 +155,13 @@ class PrescriptionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Receipt  $receipt
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         try {
-            $data = Prescription::findOrFail($id);
+            $data = Receipt::findOrFail($id);
             $data->delete();
             $ret = [
                 'success' => true,
@@ -214,14 +179,14 @@ class PrescriptionController extends Controller
     public function removeMedicine($id, $medi_id)
     {
         try {
-            $prescription = Prescription::findOrFail($id);
-            $medicines = $prescription->medicines()->detach($medi_id);
+            $receipt = Receipt::findOrFail($id);
+            $medicines = $receipt->medicines()->detach($medi_id);
             
-            if ($prescription->save()) {
+            if ($receipt->save()) {
                 $ret = [
                     'success' => true,
                     'message' => 200,
-                    'prescription' => $prescription
+                    'prescription' => $receipt
                 ];
             } else {
                 $ret = [
