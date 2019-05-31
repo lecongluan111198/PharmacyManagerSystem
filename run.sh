1965a587979177d7bf39f24908ba19f350cba8ba
beohoang98 <beohoang98@users.noreply.github.com>
2019-05-31 23:39:10 +0700

    fix vue.config

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ cd "$WORKING_DIR/frontend"
 npm run build
 
 cd "$WORKING_DIR/backend"
-export APP_KEY = ${$(php artisan key:generate --show)#base64:}
-echo $API_KEY
+export APP_KEY=${$(php artisan key:generate --show)#base64:}
+echo $APP_KEY
 
 cd "$WORKING_DIR"
 vendor/bin/heroku-php-apache2 backend/public/
