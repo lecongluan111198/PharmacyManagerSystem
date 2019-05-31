b2ba7bf4700fbf52f7e2c48f3e8561877dec6d2d
beohoang98 <beohoang98@users.noreply.github.com>
2019-06-01 00:51:20 +0700

    reconfig bash

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 WORKING_DIR=`pwd`
 
-cd "$WORKING_DIR/frontend"
-npm run build
+# cd "$WORKING_DIR/frontend"
+# npm run build
 
 cd "$WORKING_DIR/backend"
 export APP_KEY=$(php artisan key:generate --show)
 echo $APP_KEY
+php artisan passport:install
 php artisan config:cache
 
 cd "$WORKING_DIR"
